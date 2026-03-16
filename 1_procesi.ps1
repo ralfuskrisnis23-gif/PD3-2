0f4f44f9576c5bddf8143e308b02494f8c6537d1
ralfuskrisnis23-gif <ralfuskrisnis23@gmail.com>
2026-03-16 13:58:54 +0200

    Update 1_procesi.ps1

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,4 +1,3 @@
-powershell -ExecutionPolicy Bypass -File ".\1_procesi.ps1"
 $Fails = "$env:USERPROFILE\Documents\LielieProcesi.csv"
 $Procesi = Get-Process | Where-Object { $_.WorkingSet64 -gt 150MB -and $_.Name -ne "msedge" }
 $Procesi | Select-Object Name, Id, WorkingSet | Export-Csv $Fails -NoTypeInformation
