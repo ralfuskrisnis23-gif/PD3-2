646e4d21c9a63116537489a7eacbddfe24cd5022
ralfuskrisnis23-gif <ralfuskrisnis23@gmail.com>
2026-03-16 13:38:46 +0200

    Update 2_zurnals.ps1

--- 2_zurnals.ps1
+++ 2_zurnals.ps1
@@ -1,1 +1,4 @@
-
+$Fails = "$env:USERPROFILE\Documents\Warnings.txt"
+$Brindinajumi = Get-EventLog -LogName Application -EntryType Warning -After (Get-Date).AddDays(-3)
+$Brindinajumi | Out-File $Fails
+$Brindinajumi | Group-Object Source | Sort-Object Count -Descending | Select-Object -First 3 | Out-File $Fails -Append
