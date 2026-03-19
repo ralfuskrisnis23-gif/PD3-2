4b1bdb9fc46e5028abd5448be8802d8a8ec80972
ralfuskrisnis23-gif <ralfuskrisnis23@gmail.com>
2026-03-19 10:38:36 +0200

    Update 5_programmatura.ps1

--- 5_programmatura.ps1
+++ 5_programmatura.ps1
@@ -1,2 +1,2 @@
 $Programmas = winget list | Where-Object { $_ -match "msstore" }
-Write-Host "Sistēmā ir instalētas $($Programmas.Count) aplikācijas no Microsoft Store."
+Write-Host "Sistēmā ir instalētas $($Programmas.Count) aplikācija no Microsoft Store."
