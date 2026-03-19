77c3aacbf3fe3a4a5f35addb661aba8c91a157d1
ralfuskrisnis23-gif <ralfuskrisnis23@gmail.com>
2026-03-19 10:29:55 +0200

    Update 4_arhivs.ps1

--- 4_arhivs.ps1
+++ 4_arhivs.ps1
@@ -1,3 +1,3 @@
 $Arhivs = "$env:USERPROFILE\Documents\PDF_Backup.zip"
 $PDF = Get-ChildItem "$env:USERPROFILE\Downloads" -Filter *.pdf | Where-Object { $_.LastWriteTime -gt (Get-Date).AddHours(-48) }
-Compress-Archive -Path $PDF.FullName -DestinationPath $Arhivs -Force
+Compress-Archive -Path $PDF.FullName -DestinationPath $Arhivs -Forc
