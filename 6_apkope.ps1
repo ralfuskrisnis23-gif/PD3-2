e33d702abfc7f42b95e6506699e4b973a6833b9d
ralfuskrisnis23-gif <ralfuskrisnis23@gmail.com>
2026-03-19 10:53:30 +0200

    Update 6_apkope.ps1

--- 6_apkope.ps1
+++ 6_apkope.ps1
@@ -2,5 +2,5 @@ $Fails = "$env:USERPROFILE\Documents\Servisi.log"
 $Laiks = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 foreach ($Serviss in @("Spooler", "wuauserv")) {
 $Statuss = (Get-Service -Name $Serviss).Status
-Add-Content $Fails "[$Laiks] Serviss $Serviss ir $Statuss"
+Add-Content $Fails "[$Laiks] Serviss $Serviss ir $Status"
 }
